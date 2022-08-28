a894e0b0166c45463a03658308497b2adc3f12db
Seth David Gyimah <esec26@gmail.com>
2022-08-28 17:57:12 +0000

    inheritance lab

---
+++ lib/user.rb
@@ -0,0 +1,12 @@
+class User
+
+    attr_accessor :first_name, :last_name
+
+    # def initialize(name)
+    #     first_name, last_name = name.split
+    #     @first_name = first_name
+    #     @last_name = last_name
+    # end
+
+end
+
